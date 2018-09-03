52e4f655403fed2ea65fd39f1adf9b77fc82562d
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-09-03 10:38:04 -0300

    Changed the repo and add gitignore
    Clean the project to start the development

--- stories/Main.js
+++ stories/Main.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import Component from '../src/Main'; // This is our component
+import FullHeader from '../src/Main'; // This is our component
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Component', module)
-    .add('with lyef name', () => (
-        <Component name="lyef" />
-    ))
-    .add('with another name', () => (
-        <Component name="another" />
-    ))
+storiesOf('React Full Header', module);
